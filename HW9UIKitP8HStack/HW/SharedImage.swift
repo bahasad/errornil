b4078db7cef8e5939d7d823462bcb12c7d14617b
Baha Sadyr <>
2024-08-14 18:50:20 -0500

    test

--- HW9UIKitP8HStack/HW/SharedImage.swift
+++ HW9UIKitP8HStack/HW/SharedImage.swift
@@ -16,6 +16,7 @@ class SharedImage: UIImageView {
         clipsToBounds = true
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
