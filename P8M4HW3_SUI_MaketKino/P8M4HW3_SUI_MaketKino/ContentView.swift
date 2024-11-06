962a5593074344d27e8c9c4b4d2cce090681af17
Baha Sadyr <>
2024-11-06 12:15:45 +0500

    added skeleton HW3

---
+++ P8M4HW3_SUI_MaketKino/P8M4HW3_SUI_MaketKino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P8M4HW3_SUI_MaketKino
+//
+//  Created by Baha Sadyr on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P8M4HW3_SUI_MaketKino/P8M4HW3_SUI_MaketKino/P8M4HW3_SUI_MaketKinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  P8M4HW3_SUI_MaketKinoApp.swift
+//  P8M4HW3_SUI_MaketKino
+//
+//  Created by Baha Sadyr on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct P8M4HW3_SUI_MaketKinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
