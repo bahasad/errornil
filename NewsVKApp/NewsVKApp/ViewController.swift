d231bb8a5d33bba1f2622d78a7a9827cd6b69c75
Baha Sadyr <>
2024-12-04 17:28:55 +0500

    Added initial project structure HW news VK app

---
+++ NewsVKApp/NewsVKApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsVKApp
+//
+//  Created by Baha Sadyr on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red.withAlphaComponent(0.1)
+    }
+
+
+}
+
